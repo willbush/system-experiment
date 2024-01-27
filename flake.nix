247003a9e7679fcb530e293b6a3d77dcacde92e0
Will Bush <will.g.bush@gmail.com>
2024-01-26 21:29:48 -0600

    Copy over files from ex-nixos-starter-config

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Experimenting with impermanence and wayland";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    impermanence.url = "github:nix-community/impermanence";
+  };
+
+  outputs = inputs@{ nixpkgs, impermanence, home-manager, ... }: {
+    nixosConfigurations = {
+      blitzar = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        # Pass inputs into the NixOS module system
+        specialArgs = { inherit inputs; };
+
+        modules = [
+          impermanence.nixosModules.impermanence
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.will = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
