69aadb7e0eeadcc76ec26f6a673d93e081a67f1c
Will Bush <will.g.bush@gmail.com>
2024-02-03 23:21:51 -0600

    move hyprland settings to conf file
    and add some settings

--- home.nix
+++ home.nix
@@ -79,39 +79,7 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
-    settings = {
-      "$mod" = "SUPER";
-
-      bind =
-        [
-          # These are Colemak-DH keys. I don't dedicated number row unless I go
-          # through a layer key. So I rather use keys close to the home row.
-
-          # binds $mod + {q..g} to workspace {q..g}
-          "$mod, q, workspace, 1"
-          "$mod, w, workspace, 2"
-          "$mod, f, workspace, 3"
-          "$mod, p, workspace, 4"
-          "$mod, b, workspace, 5"
-          "$mod, a, workspace, 6"
-          "$mod, r, workspace, 7"
-          "$mod, s, workspace, 8"
-          "$mod, t, workspace, 9"
-          "$mod, g, workspace, 10"
-
-          # binds $mod + shift + {q..g} to move to workspace {q..g}
-          "$mod SHIFT, q, movetoworkspace, 1"
-          "$mod SHIFT, w, movetoworkspace, 2"
-          "$mod SHIFT, f, movetoworkspace, 3"
-          "$mod SHIFT, p, movetoworkspace, 4"
-          "$mod SHIFT, b, movetoworkspace, 5"
-          "$mod SHIFT, a, movetoworkspace, 6"
-          "$mod SHIFT, r, movetoworkspace, 7"
-          "$mod SHIFT, s, movetoworkspace, 8"
-          "$mod SHIFT, t, movetoworkspace, 9"
-          "$mod SHIFT, g, movetoworkspace, 10"
-        ];
-    };
+    extraConfig = builtins.readFile ./configs/hypr/hyprland.conf;
   };
 
   # Nicely reload system units when changing configs
