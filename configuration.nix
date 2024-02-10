7c9d91624fe1d429e883eb5c5d8be163a81ab5ab
Will Bush <will.g.bush@gmail.com>
2024-02-10 17:15:58 -0600

    remove settings that I don't understand / need?
    Might look into this later. This came from the starter template

--- configuration.nix
+++ configuration.nix
@@ -3,26 +3,10 @@ let hashedPassword = "$6$E9Vvt03Z9YlvNNzv$JV4K5PK3J6oqr5ALg5yb72yUzIlxA.VlWWC5Nq
 in
 {
   imports = [
+    ./greetd.nix
     ./hardware-configuration.nix
     ./persist.nix
-    ./greetd.nix
   ];
-
-  # This will add each flake input as a registry
-  # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: { inherit flake; })) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
-  # This will additionally add your inputs to the system's legacy channels
-  # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = [ "/etc/nix/path" ];
-
-  environment.etc =
-    lib.mapAttrs'
-      (name: value: {
-        name = "nix/path/${name}";
-        value.source = value.flake;
-      })
-      config.nix.registry;
 
   nix.settings = {
     experimental-features = "nix-command flakes";
